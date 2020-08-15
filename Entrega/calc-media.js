6890e6a70b27fc382fbac01e39046968545f741d
luzialeticia <leticialuzia@tutamail.com>
2020-08-15 20:14:15 -0300

    Escrevendo o código if else

--- Entrega/calc-media.js
+++ Entrega/calc-media.js
@@ -7,3 +7,13 @@ const nota3 = parseFloat(readline.question("Digite a 3ª nota: "))
 const nota4 = parseFloat(readline.question("Digite a 4ª nota: "))
 const nota5 = parseFloat(readline.question("Digite a 5ª nota: "))
 const media = (nota1+nota2+nota3+nota5)/5
+
+console.log(`A média das notas é: ${media}`)
+
+if (media >= 7) {
+    console.log(`${nome} está aprovade!`)
+}else if (media >= 5 && media < 7) {
+    console.log(`${alune} está de recuperação.`)
+}else {
+    console.log(`${alune} está reprovade.`)
+}
