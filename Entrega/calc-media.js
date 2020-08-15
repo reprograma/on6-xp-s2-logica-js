ae4df552612c5988d5c3d0be5976108df0068a63
luzialeticia <leticialuzia@tutamail.com>
2020-08-15 20:08:16 -0300

    Declarando as váriaveis

--- Entrega/calc-media.js
+++ Entrega/calc-media.js
@@ -1,1 +1,9 @@
 const input = require("readline-sync");
+
+const alune = readline.question("Digite o nome de alune: ")
+const nota1 = parseFloat(readline.question("Digite a 1ª nota: "))
+const nota2 = parseFloat(readline.question("Digite a 2ª nota: "))
+const nota3 = parseFloat(readline.question("Digite a 3ª nota: "))
+const nota4 = parseFloat(readline.question("Digite a 4ª nota: "))
+const nota5 = parseFloat(readline.question("Digite a 5ª nota: "))
+const media = (nota1+nota2+nota3+nota5)/5
