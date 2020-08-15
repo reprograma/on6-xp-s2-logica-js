441fc74c8be93c73cb4732e151f68e3f0dc2d192
Pascoa21 <jesssyca_pascoa@hotmail.com>
2020-08-15 17:51:08 -0300

    Calculadora para calculo da média

---
+++ Entrega/Calculadora_Media.js
@@ -0,0 +1,18 @@
+const readline = require('readline-sync')
+const nota1 = parseFloat(readline.question('Digite a primeira nota? '))
+const nota2 = parseFloat(readline.question('Digite a segunda nota? '))
+const nota3 = parseFloat(readline.question('Digite a terceira nota? '))
+const nota4 = parseFloat(readline.question('Digite a quarta nota? '))
+const nota5 = parseFloat(readline.question('Digite a quinta nota? '))
+
+let media = [(nota1+nota2+nota3+nota4+nota5)/5]
+
+console.log (`A media do alunx foi ${media}`)
+
+if (media >= 7)
+{
+    console.log(`A média é ${media},  APROVADO`)
+}else if ( 5>= media < 7  ){
+    console.log(`A média é ${media},  RECUPERAÇÃO`)
+}else { console.log (`A média é ${media},  REPROVADO`)}
+
