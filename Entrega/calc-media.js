685c3b27f71232a73bfd0d054b8504dc4dacd7a7
luzialeticia <leticialuzia@tutamail.com>
2020-08-15 20:17:28 -0300

    Simplificando o código, exibindo a média junto com o situação da aluna

--- Entrega/calc-media.js
+++ Entrega/calc-media.js
@@ -1,6 +1,6 @@
 const input = require("readline-sync");
 
-const alune = readline.question("Digite o nome de alune: ")
+const alune = readline.question("Digite seu nome: ")
 const nota1 = parseFloat(readline.question("Digite a 1ª nota: "))
 const nota2 = parseFloat(readline.question("Digite a 2ª nota: "))
 const nota3 = parseFloat(readline.question("Digite a 3ª nota: "))
@@ -8,12 +8,10 @@ const nota4 = parseFloat(readline.question("Digite a 4ª nota: "))
 const nota5 = parseFloat(readline.question("Digite a 5ª nota: "))
 const media = (nota1+nota2+nota3+nota5)/5
 
-console.log(`A média das notas é: ${media}`)
-
 if (media >= 7) {
-    console.log(`${nome} está aprovade!`)
+    console.log(`${alune}, sua média é: ${media}. Parabéns! Você está aprovade!`)
 }else if (media >= 5 && media < 7) {
-    console.log(`${alune} está de recuperação.`)
+    console.log(`${alune}, sua média é: ${media}. Você está de recuperação.`)
 }else {
-    console.log(`${alune} está reprovade.`)
+    console.log(`${alune}, sua média é: ${media}. Você está reprovade.`)
 }
