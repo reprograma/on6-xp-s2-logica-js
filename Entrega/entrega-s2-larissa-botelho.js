e45ae68908a19248c0ade481f56f63875b667680
botelholarissa <conta.larissa@gmail.com>
2020-08-17 18:04:33 -0300

    adicionando quebra de linha

--- Entrega/entrega-s2-larissa-botelho.js
+++ Entrega/entrega-s2-larissa-botelho.js
@@ -1,10 +1,10 @@
 const readline = require("readline-sync")
 
-const nota1 = readline.question("Oi! Qual é a nota da sua primeira prova?")
-const nota2 = readline.question("Agora insira sua nota da segunda prova.")
-const nota3 = readline.question("Insira a nota da sua terceira prova.")
-const nota4 = readline.question("Qual a nota quarta prova?")
-const nota5 = readline.question("E por fim, qual a nota da quinta prova?")
+const nota1 = readline.question("Oi! Qual é a nota da sua primeira prova? \n")
+const nota2 = readline.question("Agora insira sua nota da segunda prova. \n")
+const nota3 = readline.question("Insira a nota da sua terceira prova. \n")
+const nota4 = readline.question("Qual a nota quarta prova? \n")
+const nota5 = readline.question("E por fim, qual a nota da quinta prova? \n")
 
 // armazena a soma das notas inseridas
 const somanotas = parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4) + parseFloat(nota5)
