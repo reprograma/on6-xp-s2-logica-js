9dd601576deff88fea621b0ff36c4d2e23551c0a
tassilabomfim <eng.tassilabomfim@gmail.com>
2020-08-20 23:26:53 -0300

    atualização final

--- Entrega/calculadora_media.js
+++ Entrega/calculadora_media.js
@@ -1,1 +1,30 @@
 const readline = require('readline-sync')
+const nomeAluno = readline.question("Nome do Aluno: ")
+
+
+const primeiraNota = parseFloat(readline.question("Primeira Unidade: "))
+const segundaaNota = parseFloat(readline.question("Segunda Unidade: "))
+const terceiraNota = parseFloat(readline.question("Terceira Unidade: "))
+const quartaNota = parseFloat(readline.question("Quarta Unidade: "))
+const quintaNota = parseFloat(readline.question("Quinta Unidade: "))
+
+const media = (primeiraNota + segundaaNota + terceiraNota + quartaNota + quintaNota) / 5
+console.log("Resultado da Media : " + media)
+
+//
+if(media >= 7){
+    console.log("Aprovadx!!!  " + nomeAluno + "  o/ ")
+
+}else if(media < 5 ){
+   console.log("Reprovadx!!! " + nomeAluno + "  :( ")
+
+}else if(media >= 5 ){
+    console.log("Recuperação!! Estude.  " + nomeAluno + "  ;) ")
+
+}else if(media < 7 ){
+    console.log("Recuperação!! Estude.  " + nomeAluno + "  ;) ")
+
+}else{
+    console.log("Aconteceu algo errado!" + nomeAluno)
+}
+//
