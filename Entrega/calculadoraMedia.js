3510efde4c0dc3e53a90b738be81dbaca44beed3
camilapessoa <camilabentespessoa@gmail.com>
2020-08-21 01:23:03 -0300

    inserindo calculo de media

--- Entrega/calculadoraMedia.js
+++ Entrega/calculadoraMedia.js
@@ -13,3 +13,5 @@ const IIava = parseFloat(readlineSync.question('II Avaliação: '));
 const IIIava = parseFloat(readlineSync.question('III Avaliação: '));
 const IVava = parseFloat(readlineSync.question('IV Avaliação: '));
 const Vava = parseFloat(readlineSync.question('V Avaliação: '));
+
+const media = (Iava+IIava+IIIava+IVava+Vava)/5;
