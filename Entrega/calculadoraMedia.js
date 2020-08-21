93f0de60fff82aec88e60378e633d7f7c958c3ab
camilapessoa <camilabentespessoa@gmail.com>
2020-08-21 01:22:07 -0300

    inserindo variaveis

---
+++ Entrega/calculadoraMedia.js
@@ -0,0 +1,15 @@
+const readlineSync = require('readline-sync');
+
+console.log("Consulte sua média.")
+
+const nomeAluno = (readlineSync.question('Qual seu nome? '));
+const nomeDisciplina = (readlineSync.question('Qual a disciplina? ' ));
+
+const instrucao = ("Insira as notas de suas avaliações.");
+console.log(instrucao)
+
+const Iava = parseFloat(readlineSync.question('I Avaliação: '));
+const IIava = parseFloat(readlineSync.question('II Avaliação: '));
+const IIIava = parseFloat(readlineSync.question('III Avaliação: '));
+const IVava = parseFloat(readlineSync.question('IV Avaliação: '));
+const Vava = parseFloat(readlineSync.question('V Avaliação: '));
