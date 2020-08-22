f1e0254536ab62bd4c9513d9e17efddb34dc4143
rose <rosefelix99@gmail.com>
2020-08-21 21:59:42 -0300

    Cálculo de média

--- Entrega/rose-felix/script.js
+++ Entrega/rose-felix/script.js
@@ -7,4 +7,12 @@ const nota4 = parseInt(readline.question("Qual foi a sua quarta nota? "))
 const nota5 = parseInt(readline.question("Qual foi a sua quinta nota? "))
 
 const media = (nota1 + nota2 + nota3 + nota4 + nota5)/5
-console.log(nota1, note2, nota3, nota4, nota5, media)
+console.log(nota1, nota2, nota3, nota4, nota5, media)
+
+if (media >= 7){
+    console.log("Sua média foi " + media + ". Parabéns, você foi aprovado")
+}else if (media < 5){
+    console.log("Sua média foi " + media + ". Estudando mais um pouquinho conseguiremos, mas no momento infelizmente você está reprovade.")
+}else{
+    console.log("Sua média foi " + media + ". Ainda não fomos aprovado, mas ainda há uma chance. Vamos de recuperação!")
+}
